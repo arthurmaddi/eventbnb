693573906927ddd74d6465c002a8df5afea92b40
Nicolas Guillon <nicolas.guillon95@yahoo.com>
2024-02-26 16:28:20 +0100

    add bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,26 @@
 class BookingsController < ApplicationController
+
+  before_action :set_spot, only: %i[new create]
+
   def new
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.spot = @spot
+    @booking.user = current_user
     @booking.save
+    redirect_to spot_path(@spot)
   end
 
   private
 
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_start)
+  def set_spot
+    @spot = Spot.find(params[:spot_id])
   end
 
-
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
