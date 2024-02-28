332365fe05db01735c2ae723aa3479e250cc3b8b
Nicolas Guillon <nicolas.guillon95@yahoo.com>
2024-02-28 13:50:33 +0100

    add geocode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_152948) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_113335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_152948) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "images"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
