50ace9a4d9296e3d0017dbe863a3fcdc841929a3
a.m. <arthurmaddi59270@gmail.com>
2024-02-29 12:51:54 +0100

    reservation done

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -12,6 +12,13 @@ class SpotsController < ApplicationController
   def show
     @spot = Spot.find(params[:id])
     @booking = Booking.new
+    @bookings       = @spot.bookings
+    @bookings_dates = @bookings.map do |booking|
+      {
+        from: booking.start_date,
+        to:   booking.end_date
+      }
+    end
   end
 
 
@@ -29,31 +36,31 @@ class SpotsController < ApplicationController
     end
   end
 
-  def destroy
-    @spot = Spot.find(params[:id])
-    if @spot.user == current_user
-      @spot.destroy
-      redirect_to root_path, notice: 'Le spot a bien été supprimé.'
-    else
-      redirect_to root_path, alert: 'Vous ne pouvez pas supprimer ce spot.'
+    def destroy
+      @spot = Spot.find(params[:id])
+      if @spot.user == current_user
+        @spot.destroy
+        redirect_to root_path, notice: 'Le spot a bien été supprimé.'
+      else
+        redirect_to root_path, alert: 'Vous ne pouvez pas supprimer ce spot.'
+      end
     end
-  end
 
-  def edit
-    @spot = Spot.find(params[:id])
-  end
-
-  def update
-    @spot = Spot.find(params[:id])
-
-
-    if @spot.user == current_user
-      @spot.edit(spot_params)
-      redirect_to spot_path(@spot)
-    else
-      redirect_to root_path, alert: 'Vous ne pouvez pas modifier ce spot.'
+    def edit
+      @spot = Spot.find(params[:id])
     end
-  end
+
+    def update
+      @spot = Spot.find(params[:id])
+
+
+      if @spot.user == current_user
+        @spot.edit(spot_params)
+        redirect_to spot_path(@spot)
+      else
+        redirect_to root_path, alert: 'Vous ne pouvez pas modifier ce spot.'
+      end
+    end
 
 
 
